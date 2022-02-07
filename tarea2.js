25f598d4850ddc0a0b2326062ca306b61d260908
César Sipaque <cesar@inblackmark.com>
2022-02-07 13:47:40 -0600

    Agregados archivos de tarea y puntos extras unidad 2

---
+++ tarea2.js
@@ -0,0 +1,12 @@
+const mes = 10;
+
+if( mes=12 || mes<=2 ){
+  alert("es invierno");
+}else if( mes>=3 && mes<=5 ){
+  alert("es primavera");
+}else if( mes>=6 && mes<=8 ){
+  alert("es verano");
+}else if( mes>=9 && mes<=11 ){
+  alert("es otoño");
+}
+alert('22001793\r\n César Emanuel Sipaque');
